e91bee11d2cd8dae0dea47db82a7ac7419b9b814
Sandra Połujańska <sandra.polujanska@gmail.com>
2018-03-02 07:41:21 +0100

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// scripts.js
+
+
+function drawTree(h) {
+    for (var i=0; i<=h; i++) {
+        var star= '';
+        
+        for (var j=0; j<i; j++) {
+            star += '*';
+        }
+        console.log(star);
+    }
+        
+}
+drawTree(5);
