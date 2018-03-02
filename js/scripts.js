d764b1f3cd049d519d4fe8d39c43afa2a27c96ea
Sandra Połujańska <sandra.polujanska@gmail.com>
2018-03-02 13:09:20 +0100

    Change tree js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,13 @@
 function drawTree(h) {
     for (var i=0; i<=h; i++) {
         var star= '';
-        
-        for (var j=0; j<i; j++) {
-            star += '*';
+        for (var s=h-i; s>=0; s--) {
+            star += ' ';
         }
-        console.log(star);
+        for (var j=0; j<i; j++) {
+            star += '**';
+        }
+        console.log(star + '*');
     }
         
 }
